748796765376bfcde91ee8f40f4485359620d25d
koehlersimon <info@simon-koehler.com>
2021-10-07 10:28:20 -0500

    Fix path

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'uploadfolder' => '0',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.2.99'
+            'typo3' => '10.4.0-11.5.99'
         ],
         'conflicts' => [
 
