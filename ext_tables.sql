5c798ddc2e15228a022a2186df98d4da3fbd5f73
koehlersimon <info@simon-koehler.com>
2021-10-07 10:24:04 -0500

    Database fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,5 @@
 CREATE TABLE tt_content (
     counter_time int(11) unsigned DEFAULT '0' NOT NULL,
-    counter_direction int(1) unsigned DEFAULT '0' NOT NULL,
+    counter_stop int(11) unsigned DEFAULT '0' NOT NULL,
+    counter_stop_text text,
 );
