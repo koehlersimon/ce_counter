041e081991aa1197f41ef607707319c66ce6be83
koehlersimon <info@simon-koehler.com>
2021-10-07 10:28:41 -0500

    Script and version update

--- Resources/Public/Js/Countdown.js
+++ Resources/Public/Js/Countdown.js
@@ -6,16 +6,35 @@ document.addEventListener('DOMContentLoaded',function(){
         let countdownInterval = setInterval(function(){
             let now = new Date().getTime();
             countdownInstances.forEach((countdownInstance, i) => {
-                let countDownTime = new Date((countdownInstance.getAttribute('data-countdown') * 1000));
-                let timeleft = countDownTime - now;
-                var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-                countdownInstance.querySelector('.days').innerHTML = days + "d ";
-                countdownInstance.querySelector('.hours').innerHTML = hours + "h ";
-                countdownInstance.querySelector('.mins').innerHTML = minutes + "m ";
+
+                let countDownTime = new Date((countdownInstance.getAttribute('data-time') * 1000));
+                let displayTime;
+
+                if(countDownTime > now){
+                    displayTime = countDownTime - now;
+                }
+                else{
+                    displayTime = now - countDownTime;
+                }
+
+                let days = Math.floor(displayTime / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((displayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor((displayTime % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((displayTime % (1000 * 60)) / 1000);
+
+                if(countdownInstance.hasAttribute('data-initialized') === false){
+                    countdownInstance.querySelector('.days').innerHTML = days + "d ";
+                    countdownInstance.querySelector('.hours').innerHTML = hours + "h ";
+                    countdownInstance.querySelector('.mins').innerHTML = minutes + "m ";
+                    countdownInstance.setAttribute('data-initialized','true');
+                }
+
                 countdownInstance.querySelector('.secs').innerHTML = seconds + "s ";
+
+                if(seconds === 1 || seconds === 59){
+                    countdownInstance.querySelector('.mins').innerHTML = minutes + "m ";
+                }
+
             });
         }, 1000);
     }
